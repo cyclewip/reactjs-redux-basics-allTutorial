4eb99a42490316b6bbbc01f8768996e72e81ccf2
Sandelin <martin.sandelin@capgemini.com>
2018-07-30 16:58:05 +0200

    last change 07/30

--- examples/todos/src/reducers/cars.js
+++ examples/todos/src/reducers/cars.js
@@ -1,21 +1,25 @@
-const initialState = [
-    {id: 0, text: 'BMW'},
-    {id: 1, text: 'Volvo'},
-    {id: 2, text: 'Jaguar'}
-  ]
+// const initialState = [
+//     {id: 0, text: 'BMW'},
+//     {id: 1, text: 'Volvo'},
+//     {id: 2, text: 'Jaguar'}
+//   ]
   
-//   const initialState = {
-//     cars : [
-//         {id: "0",text: "BMW"},
-//         {id: "1",text: "Volvo"},
-//         {id: "2",text: "Mazda"}
-//     ]
-// }
+const initialState = {
+    cars : [
+        {id: "0",text: "BMW"},
+        {id: "1",text: "Volvo"},
+        {id: "2",text: "Mazda"}
+    ]
+}
 const cars = (state = initialState, action) => {
     switch (action.type) {
       case 'ADD_CAR':
         return [
           ...state,
+        //   state.cars = {
+        //     ...state.cars),
+        //     ...state.cars.slice(action.id + 1) 
+        // }
           {
             id: action.id,
             text: action.text,
@@ -24,9 +28,9 @@ const cars = (state = initialState, action) => {
     case 'REMOVE_CAR':
       return [
         ...state,
-        {
-          id: action.id,
-          text: action.text
+        cars = {
+            ...state.cars.slice(0, action.id),
+            ...state.cars.slice(action.id + 1) 
         }
       ]
       default:
